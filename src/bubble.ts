fde81ca1a9bff27b2e2bd69d6eea2cade8554a74
soramar <fujitanisora0414@i.softbank.jp>
2023-10-07 21:41:56 +0900

    .

--- src/bubble.ts
+++ src/bubble.ts
@@ -15,5 +15,3 @@ const bubble_sort = (nums: number[]): void => {
 const nums: number[] = [2, 5, 1, 8, 7, 3]
 
 bubble_sort(nums)
-
-// 要素数を取得し、1ループごとに比較の開始点を左にずらす。j = 0から右向きに値を比較していき、比較終了後のループでインクリメントをすることでに要素の比較を1要素ずつずらす:w
